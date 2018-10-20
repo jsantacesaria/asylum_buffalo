0f0947200522f4c327609088eb22c0d786933d52
EC2 Default User <ec2-user@ip-172-31-16-162.ec2.internal>
2018-10-20 19:55:44 +0000

    added home button to contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -54,6 +54,7 @@ body, html {
 
 <div class="bg-text">
     <h1 style="font-size:40px">Asylum Contact Form Coming Soon</h1>
+    <button type = "button" onclick="location.href='http://www.asylumbuffalo.com'">Home</button>
 
 </div>
 
