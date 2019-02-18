e2d24e3a3858a4d9c823ea553587df39a7ea031c
EC2 Default User <ec2-user@ip-172-31-16-162.ec2.internal>
2019-02-18 19:19:49 +0000

    Edited about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,15 +11,14 @@
 
 <div>
     <h1 >About Asylum</h1>
-    <p>Located in the Buffalo and Greater Western New York area, Asylum is a private, voluntary association for adults (18 and older) with an interest in 
-	responsibly exploring the BDSM lifestyle. 
+    <p>Located in Western New York, Asylum is a private, voluntary association for adults (18 and older) interested in 
+	responsibly exploring BDSM. 
 	We value the central tenants of ethical BDSM - Safe, Sane and Consensual(SSC), Personal Responsibility Informed Consensual Kink (PRICK) 
 	and Risk Aware Consensual Kink( RACK). 
-	Our core values revolve around respect, confidentiality and consent (Did we mention consent?).
+	Our core values revolve around respect, confidentiality and consent.
 	We are proudly a Coalition Partner of the 
 	<a href="https://ncsfreedom.org/who-we-are/coalition-partners" style="color:#FF0000;" target="_blank">NCSF(National Coalition of Sexual Freedom)</a>. 
-	 Curious? Interested in learning more? Want to become a member? 
-	Please <a href="contact" style="color:#FF0000;" >contact us!</a>
+	 Interested in learning more? Please <a href="contact" style="color:#FF0000;" >contact us!</a>
     </p> 
     <button type = "button" onclick="location.href='http://www.asylumbuffalo.com'">Home</button>	
     <button type = "button" onclick="location.href='contact'">Contact Us</button>
