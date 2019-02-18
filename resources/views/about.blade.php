ee1a10aee494a41cfe514add2eea70e656f735cc
EC2 Default User <ec2-user@ip-172-31-16-162.ec2.internal>
2019-02-18 22:28:40 +0000

    Refining about content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,14 +11,14 @@
 
 <div>
     <h1 >About Asylum</h1>
-    <p>Located in Western New York, Asylum is a private, voluntary association for adults (18 and older) interested in 
+    <p>Located in Western NY, Asylum is a private, voluntary association for adults interested in 
 	responsibly exploring BDSM. 
 	We value the central tenants of ethical BDSM - Safe, Sane and Consensual(SSC), Personal Responsibility Informed Consensual Kink (PRICK) 
 	and Risk Aware Consensual Kink( RACK). 
-	Our core values revolve around respect, confidentiality and consent.
+	Our core values include respect, confidentiality and consent.
 	We are proudly a Coalition Partner of the 
 	<a href="https://ncsfreedom.org/who-we-are/coalition-partners" style="color:#FF0000;" target="_blank">NCSF(National Coalition of Sexual Freedom)</a>. 
-	 Interested in learning more? Please <a href="contact" style="color:#FF0000;" >contact us!</a>
+
     </p> 
     <button type = "button" onclick="location.href='/'">Home</button>	
     <button type = "button" onclick="location.href='contact'">Contact Us</button>
