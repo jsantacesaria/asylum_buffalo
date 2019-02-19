8d465b38b8fc1dc4bf956116f89c8ba2b595930b
EC2 Default User <ec2-user@ip-172-31-16-162.ec2.internal>
2019-02-19 01:17:35 +0000

    back end stuffs

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,8 +12,8 @@
 @section('content')
 
 <div>
-    <h1 >Asylum Members Only Login</h1>
-    <button type = "button" onclick="location.href='login'">Login</button>
+    <h1 > Coming Soon! Asylum Members Login</h1>
+    <button type = "button" onclick="location.href='about'">Login</button>
 	
 </div>
 @endsection
